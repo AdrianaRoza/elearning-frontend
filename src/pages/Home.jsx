380769d27aef5dd08f42db597e79f145a8fe88d8
Adriana Roza <adriana.roza.aka@gmail.com>
2025-11-10 09:11:43 -0400

    feat(home): add main page structure with Navbar, Hero, CourseList and Footer components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,25 @@
-import Hero from "../components/Hero"
+import React,{ useEffect, useState } from "react"
 import Navbar from "../components/Navbar"
+import Hero from "../components/Hero"
+import CourseList from "../components/CourseList"
+import Footer from "../components/Footer"
+import { fetchCourses } from "../services/api"
+
 
 
 export default function Home() {
+  const [courses, setCourses] = useState([])
+
+  useEffect(() => {
+    fetchCourses().then(data => setCourses(data))
+  }, [])
 
   return (
     <>
       <Navbar />
       <Hero />
-      
-      
+      <CourseList courses={courses} />
+      <Footer />  
     </>
   )
 }
